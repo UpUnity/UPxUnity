7802b5ad9e8ecd4ff44c4a405ce6653b9109af6b
rodrigorvsn <rodrigovictor81@gmail.com>
2023-11-23 00:04:25 -0300

    feat(ui): add button controller and textures

--- Assets/Scripts/TargetCollider.cs
+++ Assets/Scripts/TargetCollider.cs
@@ -1,10 +1,39 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TargetCollider : MonoBehaviour
 {
+    private string clickedButtonTag;
+    private int points = 0;
+
+    public Text pointsText; // Reference to a UI Text component to display points
+
+    public void SetClickedButton(string buttonTag)
+    {
+        clickedButtonTag = buttonTag;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         GameObject otherObject = other.gameObject;
-        Debug.Log("Collided with tag: " + otherObject.tag);
+
+        if (otherObject.CompareTag(clickedButtonTag))
+        {
+            IncrementPoints();
+        }
+    }
+
+    private void IncrementPoints()
+    {
+        points += 10;
+        UpdatePointsText();
+    }
+
+    private void UpdatePointsText()
+    {
+        if (pointsText != null)
+        {
+            pointsText.text = points.ToString();
+        }
     }
 }
